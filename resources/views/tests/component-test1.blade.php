452410093af7b625073ac094df4acf844ffa00d0
jumpei arai <jumpei0924@gmail.com>
2024-04-30 17:54:53 +0900

    props

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -2,5 +2,6 @@
   <x-slot name="header">ヘッダー1</x-slot>
 コンポーネントテスト１
 
-  <x-tests.card title="タイトル" content="コンテンツ" :message="$message"></x-tests.card>
+  <x-tests.card title="タイトル1" content="コンテンツ" :message="$message"></x-tests.card>
+  <x-tests.card title="タイトル2" ></x-tests.card>
 </x-tests.app>
