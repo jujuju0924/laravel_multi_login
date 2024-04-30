a0b7be3f85fcbcf136949a9c18fc8c2b58bc9068
jumpei arai <jumpei0924@gmail.com>
2024-04-30 18:36:09 +0900

    classBase

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,3 +1,6 @@
 <x-tests.app>
   コンポーネントテスト2
+  <x-test-class-base classBaseMessage="メッセージ"/>
+  <div class="mb-4"></div>
+  <x-test-class-base classBaseMessage="メッセージ" defaultMessage="初期値から変更しています"/>
 </x-tests.app>
