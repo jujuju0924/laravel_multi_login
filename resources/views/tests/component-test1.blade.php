257c021327cd46bf0fc42ca77262e35db3e01ecd
jumpei arai <jumpei0924@gmail.com>
2024-04-30 18:11:24 +0900

    属性バッグ

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -4,4 +4,5 @@
 
   <x-tests.card title="タイトル1" content="コンテンツ" :message="$message"></x-tests.card>
   <x-tests.card title="タイトル2" ></x-tests.card>
+  <x-tests.card title="CSSを変更したい" class="text-red-300" />
 </x-tests.app>
